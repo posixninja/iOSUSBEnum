59b4122143f2a61ada10da5f7b35f93c0918e6aa
Joshua Hill <posixninja@gmail.com>
2013-01-26 16:15:03 -0500

    Initial Commit

---
+++ iosusbenum/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  iosusbenum
+//
+//  Created by Joshua Hill on 1/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+	return 0;
+}
+
